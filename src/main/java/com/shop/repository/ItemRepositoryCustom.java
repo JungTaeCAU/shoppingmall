39861c221b2a2e180058cbd2dbd5a37bcc123695
JungTae <ddee1212@naver.com>
2021-12-21 03:57:16 +0800

    Initial commit

---
+++ src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -0,0 +1,14 @@
+package com.shop.repository;
+
+import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
+import com.shop.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ItemRepositoryCustom {
+
+    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDtom, Pageable pageable);
+
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+}
